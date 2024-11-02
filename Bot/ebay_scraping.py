20ac3136a98ca043ff832c0e93c7633e56d97d7b
zlgjaway <long252005@gmail.com>
2024-11-02 14:34:26 +1030

    Third commit

--- Bot/ebay_scraping.py
+++ Bot/ebay_scraping.py
@@ -10,29 +10,36 @@ API_key =os.getenv("api_key")
 class EbayScraper(object):
     def __init__(self, API_key):
         self.api_key = API_key
+        self.max_price = 500
+        self.min_price = 10
+        self.limit_rate = 5000
 
     def extract_csv(self):
-        df = pd.read_csv('facebook_marketplace_data.csv', sep=',', usecols=['title'])
-        keywords = df['title'].tolist()  
+        df = pd.read_csv('facebook_marketplace_data.csv', sep=',', usecols=['title','price'])
+        filtered_df  = df[(df['price'] >= self.min_price) & (df['price'] <= self.max_price)]
+        keywords =  filtered_df['title'].tolist()
         return  keywords
+    
     def request_item(self, keywords):
-        try:
-            api  = finding(appid=self.api_key, config_file=None )
-            for keyword in keywords:    
-                response = api.execute('findCompletedItems', {
-                    'keywords': keyword,  
-                    'sortOrder': 'EndTimeSoonest',    
-                    'paginationInput': {
-                    'entriesPerPage': 10,         
-                    'pageNumber': 1
-                    }
-                })
-                print (response)
-            for item in response.reply.searchResult.item:
-                print(f"Price: {item.currentPrice.value}")
-        except ConnectionError as e:
-            print(e)
-            print(e.response.dict())
+        call_rate = 0
+        while call_rate <= self.limit_rate : 
+            try:
+                api  = finding(appid=self.api_key, config_file=None )
+                for keyword in keywords:    
+                    response = api.execute('findCompletedItems', {
+                        'keywords': keyword,  
+                        'sortOrder': 'EndTimeSoonest',    
+                        'paginationInput': {
+                        'entriesPerPage': 10,         
+                        'pageNumber': 1
+                        }
+                    })
+                    print (response)
+                call_rate = call_rate +1
+                for item in response.reply.searchResult.item:
+                    print(f"Price: {item.currentPrice.value}")
+            except ConnectionError as e:
+                print(e.response.dict())
 
 scraper = EbayScraper(API_key)
 keywords = scraper.extract_csv()  # Get all keywords
--- Bot/test.py
+++ Bot/test.py
@@ -10,12 +10,23 @@ API_key =os.getenv("api_key")
 class EbayScraper(object):
     def __init__(self, API_key):
         self.api_key = API_key
-
-    def extract_csv(self):
-        df = pd.read_csv('facebook_marketplace_data.csv', sep=',', usecols=['title'])
-        keywords = df['title'].tolist()  
-        print(keywords)
-        return  keywords
+        self.max_price = 500
+        self.min_price = 10
+        self.limit_rate = 5000
+    
+    def check_api_usage(self):
+        try:
+            api = finding(appid=self.api_key, config_file=None)
+            response = api.execute('GetApiUsage', {})
+            api_calls_remaining = response.reply.ApiUsage.Remaining
+            api_calls_made = response.reply.ApiUsage.Made
+            print(f"API Calls Made: {api_calls_made}, API Calls Remaining: {api_calls_remaining}")
+        except ConnectionError as e:
+            print(f"Error checking API usage: {e.response.dict()}")
+        
+    def request_item(self):
+        self.check_api_usage()
 
 scraper = EbayScraper(API_key)
-keywords = scraper.extract_csv()
+scraper.request_item()    # Pass all keywords at once
+
