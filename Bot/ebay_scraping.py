d853ce9ab033be4a52b4553b533460d1bda4bc19
zlgjaway <long252005@gmail.com>
2024-11-08 07:59:14 +1030

    Fix the capcha pop up  commit

--- Bot/ebay_scraping.py
+++ Bot/ebay_scraping.py
@@ -11,20 +11,24 @@ import pandas as pd
 chrome_options = Options()
 chrome_options.add_argument("--disable-gpu")
 chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
 
 class EbayScrapper:
     def __init__(self):
         self.email = "long252005@gmail.com"
         self.password = "Rog#252005"
-        self.driver =  webdriver.Firefox(options=chrome_options)
+        self.driver =  webdriver.Chrome(options=chrome_options)
 
     def login(self):
         self.driver.get("https://signin.ebay.com.au/ws/eBayISAPI.dll?SignIn&sgfl=gh&ru=https%3A%2F%2Fwww.ebay.com.au%2F")
         #bot detection
         #if self.driver.find_element(By.XPATH, "//label[@for='userid']"):
-        WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//label[@for='userid']"))).send_keys("question_boi")      
-        WebDriverWait(self.driver, 5).until(EC.find_element((By.ID, "userid"))).send_keys(self.email)
-        webdriverWait(self.driver,5).until(EC.find_element((By.ID,"pass"))).send_keys(self.password)
+        
+        WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "button-submit button"))).click()
+        WebDriverWait(self.driver,5).until(EC.find_element((By.ID,"userid"))).send_keys(self.email)
+        WebDriverWait(self.driver,5).until(EC.find_element((By.ID,"pass"))).send_keys(self.password)
         time.sleep(5)
 #id="recaptcha-anchor-label">
 
@@ -41,10 +45,11 @@ class EbayScrapper:
             self.driver.find_element(By.ID,"gh-btn").click()
             self.driver.find_element(By.ID,"icon-checkbox-unchecked-18").click()
             items = []
+            #loop 10 prices tage for 1 item
             try:
                 if  items <=  10:
                     all_product_elements = self.driver.find_elements(By.CSS_SELECTOR, "a[role='link']")
-                    all_html_content.extend([elem.get_attribute('outerHTML') for elem in all_product_elements])
+                    items.extend([elem.get_attribute('outerHTML') for elem in all_product_elements])
 
             except Exception as e:
                 print(f"there are an error: {e}")
--- Bot/test.py
+++ Bot/test.py
@@ -1,3 +1,4 @@
+"""
 import os
 from ebaysdk.finding import Connection as finding
 from ebaysdk.exception import ConnectionError
@@ -51,4 +52,61 @@ scraper = EbayScraper(API_key)
 keywords = scraper.extract_csv()  # Get all keywords
 data = scraper.request_item(keywords)    # Pass all keywords at once
 scraper.save_to_csv(data)
+"""
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.chrome.options import Options
+from selenium.common.exceptions import ElementNotInteractableException
 
+import time
+from bs4 import BeautifulSoup
+import re
+import pandas as pd
+
+chrome_options = Options()
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--disable-software-rasterizer")
+chrome_options.add_argument("start-maximized")
+chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+chrome_options.add_experimental_option('useAutomationExtension', False)
+
+
+class EbayScrapper:
+    def __init__(self):
+        self.email = "long252005@gmail.com"
+        self.password = "Rog#252005"
+        self.driver =  webdriver.Chrome(options=chrome_options)
+
+    def login(self):
+        self.driver.get("https://signin.ebay.com.au/ws/eBayISAPI.dll?SignIn&sgfl=gh&ru=https%3A%2F%2Fwww.ebay.com.au%2F")
+        #bot detection
+        #if self.driver.find_element(By.XPATH, "//label[@for='userid']"):
+        
+        #WebDriverWait(self.driver, 15).until(EC.element_to_be_clickable((By.CLASS_NAME, "button-submit button"))).click()
+        #WebDriverWait(self.driver, 60).until(EC.presence_of_element_located((By.ID, "userid")))
+
+        #WebDriverWait(self.driver, 60).until(EC.element_to_be_clickable((By.ID, "userid"))).send_keys(self.email)
+        #self.driver.find_element(By.ID, "userid").send_keys(self.email)
+        #WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.XPATH, "//label[@for='userid']"))).send_keys(self.email)
+        try:
+    # Wait for the element to be clickable and attempt to interact
+            #WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.ID, "anchor-state")))
+            self.driver.find_element(By.ID, "anchor-state")
+            print("Element found")
+            input("Please complete the reCAPTCHA and press Enter to continue...")
+        except ElementNotInteractableException:
+            print("Element is not interactable")
+            self.driver.find_element(By.ID, "userid").send_keys(self.email)
+            
+        #self.driver.find_element(By.ID, "userid").send_keys(self.email)
+        self.driver.find_element(By.ID, "signin-continue-btn").click()
+        self.driver.find_element(By.ID, "pass").send_keys(self.password)
+        self.driver.find_element(By.ID, "sgnBt").click()
+        time.sleep(5)
+#id="recaptcha-anchor-l
+
+scrapper =  EbayScrapper()
+scrapper.login()
+#id="recaptcha-anchor-label">
